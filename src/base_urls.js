c2e6b1043ad6cd9e5afb706df381235491984823
Albert Karamyan <albert.karamyan@gmail.com>
2020-09-29 19:28:29 +0400

    Search for Drivers load more: missing url parts

--- src/base_urls.js
+++ src/base_urls.js
@@ -24,7 +24,7 @@ export default {
     "getIndividualUser": `${HOST}/profile_info/{0}?profile=user_profile&user_type={1}`,
     "getCalendarSettings": `${HOST}/calendar_settings/{0}`,
     "updateCalendarSettings": `${HOST}/calendar_settings/{0}`,
-    "searchForDriver": `${HOST}/search_drivers?date={0}&travelers={1}&trip_id={2}`,
+    "searchForDriver": `${HOST}/search_drivers?date={0}&travelers={1}&trip_id={2}&offset={3}&limit={4}`,
     "confirmTripBookingCheckout": `${HOST}/booked_trips`,
   }
 }
