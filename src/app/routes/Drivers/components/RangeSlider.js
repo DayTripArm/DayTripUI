8a8e8e4279fbd37cc631b63df473616ff44704b1
Albert Karamyan <albert.karamyan@gmail.com>
2020-10-25 23:42:48 +0400

    DT-237 Initial commit for Price popup and slider

---
+++ src/app/routes/Drivers/components/RangeSlider.js
@@ -0,0 +1,148 @@
+import React from "react";
+import { Grid, Button, TextField, InputAdornment } from "@material-ui/core";
+import { Slider, Rail, Handles, Tracks, Ticks } from "react-compound-slider";
+import { MuiRail, MuiHandle, MuiTrack, MuiTick } from "./components";
+import BarChart from "./BarChart";
+
+class RangeSlider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const range = [props.price_range[0], props.price_range[1]];
+
+    this.state = {
+      domain: range,
+      update: range,
+      values: range,
+      inputValues: range,
+      price_range: this.props.price_range
+    };
+      console.log(this.state)
+  }
+
+  render() {
+    const { domain, values, update, inputValues, price_range } = this.state;
+      console.log(this.state)
+    return (
+      <Grid container>
+        <Grid item xs={12}>
+          <div style={{ margin: "4%", height: 120, width: "90%" }}>
+            <BarChart
+              data={this.props.data}
+              highlight={update}
+              domain={domain}
+            />
+            <Slider
+              mode={3}
+              step={1}
+              domain={domain}
+              rootStyle={{
+                position: "relative",
+                width: "100%"
+              }}
+              onUpdate={(update) => {
+                this.setState({ update, inputValues: update})
+                this.setState({ update, price_range: update})
+              }}
+              onChange={(values) => {this.setState({ values }); this.setState({price_range: values})}}
+              values={values}
+            >
+              <Rail>
+                {({ getRailProps }) => <MuiRail getRailProps={getRailProps} />}
+              </Rail>
+              <Handles>
+                {({ handles, getHandleProps }) => (
+                  <div className="slider-handles">
+                    {handles.map((handle) => (
+                      <MuiHandle
+                        key={handle.id}
+                        handle={handle}
+                        domain={domain}
+                        getHandleProps={getHandleProps}
+                      />
+                    ))}
+                  </div>
+                )}
+              </Handles>
+              <Tracks left={false} right={false}>
+                {({ tracks, getTrackProps }) => (
+                  <div className="slider-tracks">
+                    {tracks.map(({ id, source, target }) => (
+                      <MuiTrack
+                        key={id}
+                        source={source}
+                        target={target}
+                        getTrackProps={getTrackProps}
+                      />
+                    ))}
+                  </div>
+                )}
+              </Tracks>
+              <Ticks count={5}>
+                {({ ticks }) => (
+                  <div className="slider-ticks">
+                    {ticks.map((tick) => (
+                      <MuiTick key={tick.id} tick={tick} count={ticks.length} />
+                    ))}
+                  </div>
+                )}
+              </Ticks>
+            </Slider>
+            <Grid
+              container
+              alignItems="center"
+              justify="space-around"
+              style={{ marginTop: "60px" }}
+            >
+              <Grid item xs={4} style={{ textAlign: "right" }}>
+                <TextField
+                  variant="outlined"
+                  label="min price"
+                  value={inputValues[0]}
+                  onChange={(evt) => {
+                    const value = evt.target.value;
+                    const newState = [value, inputValues[1]];
+                    this.setState({ inputValues: newState });
+                    if (value && value >= domain[0]) {
+                      this.setState({ values: newState });
+                    }
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+              <Grid item xs={4} style={{ textAlign: "center" }}>
+                —
+              </Grid>
+              <Grid item xs={4} style={{ textAlign: "left" }}>
+                <TextField
+                  variant="outlined"
+                  label="max price"
+                  value={inputValues[1]}
+                  onChange={(evt) => {
+                    const value = evt.target.value;
+                    const newState = [inputValues[0], value];
+                    this.setState({ inputValues: newState });
+                    if (value && value <= domain[1] && value >= values[0]) {
+                      this.setState({ values: newState });
+                    }
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">$</InputAdornment>
+                    )
+                  }}
+                />
+              </Grid>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default RangeSlider;
