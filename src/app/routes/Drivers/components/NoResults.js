e073f908da2449d858898dc10409c8370955fd45
Maryam Kharamyan <mkharamy@gmail.com>
2020-11-01 16:40:08 +0400

    Changed image of Not Available drivers.

--- src/app/routes/Drivers/components/NoResults.js
+++ src/app/routes/Drivers/components/NoResults.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ImpNoResults from 'assets/images/no_results.svg';
+import ImgCar from 'assets/images/car.svg';
 
 const NoResults = () => (
   <div className='text-center mh-min-screen'>
-    <img src={ImpNoResults} alt='person' className='mb-6 px-6 px-md-0' />
+    <img src={ImgCar} alt='person' className='mb-6 px-6 px-md-0' />
     <h2 className='text__blue mb-4'>There Are No Available Drivers</h2>
       <p className='text__grey-dark'>
           Please consider rescheduling your trip. You can edit the date and try again
