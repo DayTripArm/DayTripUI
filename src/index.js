03c986d8667119a23536e2ff5c89b1150a5e1c0b
Maryam Kharamyan <mkharamy@gmail.com>
2020-11-22 17:09:31 +0400

    DAYTRIP-298: Added hooked function to scroll to top in every route page.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import ReactDOM from 'react-dom';
-import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import { Switch, Route, BrowserRouter as Router, useLocation } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
@@ -14,9 +14,21 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
 sagaMiddleware.run(rootSaga);
 
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        setTimeout(() => {
+            window.scrollTo(0,0)
+        }, 200)
+    }, [pathname]);
+
+    return null;
+}
 ReactDOM.render(
     <Provider store={store}>
         <Router>
+          <ScrollToTop />
           <Switch>
             <Route path='/' component={() => <App />} />
           </Switch>
