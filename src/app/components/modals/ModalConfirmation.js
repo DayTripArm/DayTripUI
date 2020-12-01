d203bc90ac002705aabb46017eaf26f102368353
Maryam Kharamyan <mkharamy@gmail.com>
2020-12-01 13:39:50 +0400

    DAYTRIP-211: Added email section and button design for confirmation pop up

--- src/app/components/modals/ModalConfirmation.js
+++ src/app/components/modals/ModalConfirmation.js
@@ -6,10 +6,17 @@ const ModalConfirmation = () => {
     return (
         <Modal title='Verify Your Account' >
             <div className='py-4 px-0 px-md-8'>
-                    <div class="text__blue text-center mb-0">
-                        <p><h3>Check your Email</h3></p>
-                        <p><h6 className={`text-wrap`}>Please confirm your email address by clicking on the link we have sent</h6></p>
-                        <button className={`btn`}>Resend Email</button>
+                    <div className="text__blue text-center mb-0">
+                        <h3>Check your Email</h3>
+                        <h6 className={`text-wrap`}>Please confirm your email address by clicking on the link we have sent</h6>
+                        <p>&nbsp;</p>
+                        <div className="form-field mr-lg-4 mb-lg-0">
+                            <div className="position-relative">
+                                <p><input type="text" disabled="disabled" className="is-readonly" autoComplete="off" readOnly="" value="mkharamy@gmail.com"/></p>
+                            </div>
+                        </div>
+                        <p>&nbsp;</p>
+                        <button className={`btn btn-primary btn-block__md text-uppercase`}>Resend Email</button>
                     </div>
             </div>
         </Modal>
