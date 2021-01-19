97f334c3bb6c9f47011e8f7cd2bef02a79704b97
Maryam Kharamyan <mkharamy@gmail.com>
2021-01-19 14:57:46 +0400

    DAYTRIP-339: Show hide top menu (D) for mobile users. Menu is available only for Unauthorized users. Also fixed navigation to Home page and close drop down menu when user clicks on either item.

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import { IconArrowDown, IconArrowUp, IconZoom } from 'shared/components/Icons';
 import SelectCustomSearch from "../../shared/components/SelectCustomSearch";
 import {useDispatch, useSelector} from "react-redux";
+import { useHistory } from "react-router-dom";
 import useOutsideClick from 'shared/hooks/useOutsideClick';
 import HeaderUnauthorized from './fragments/HeaderUnauthorized';
 import HeaderAuthorized from './fragments/HeaderAuthorized';
@@ -31,8 +32,10 @@ const Header = ({ type = 'unauthorized', navigationType = 'user' }) => {
     };
 
     const searchContainer = useRef();
+    const history = useHistory();
 
     const HeaderSection = headerTypes[type];
+    const authorized = (type == 'unauthorized')? false : true;
 
     const [mobileSearchActive, setMobileSearchActive] = useState(false);
     const [mobileMenuActive, setMobileMenuActive] = useState(false);
@@ -55,14 +58,14 @@ const Header = ({ type = 'unauthorized', navigationType = 'user' }) => {
                         <h3 className={`d-none d-md-inline-block mb-0 weight-400${themeLight ? ' text-white' : ' text__blue'}`}>DAYTRIP</h3>
 
                         {/* Mobile Logo and Menu Toggle */}
-                        <button className={`d-md-none btn btn-circle btn-static btn-sm border-0${themeLight ? ' bg-transparent' : ''}`} onClick={() => setMobileMenuActive(!mobileMenuActive)}>
+                        <button className={`d-md-none btn btn-circle btn-static btn-sm border-0${themeLight ? ' bg-transparent' : ''}`} onClick={() => !authorized ? setMobileMenuActive(!mobileMenuActive) : () => false}>
                             <h3 className={`text-nowrap mb-0 weight-400 ${themeLight ? ' text-white' : ' text__blue'}`}>
                                 D
-                                {mobileMenuActive ? (
+                                {!authorized ? (mobileMenuActive ? (
                                     <IconArrowUp fill={themeLight ? '#FFFFFF' : '#100F72'} className='mobile-icon' />
                                 ) : (
                                     <IconArrowDown fill={themeLight ? '#FFFFFF' : '#100F72'} className='mobile-icon'/>
-                                )}
+                                )): null}
                             </h3>
                         </button>
 
@@ -81,10 +84,15 @@ const Header = ({ type = 'unauthorized', navigationType = 'user' }) => {
                         {/* Mobile Menu */}
                         <nav className={`mobile-menu d-md-none${mobileMenuActive ? '' : ' d-none'}`}>
                             <ul className='no-list-style mb-0 px-4'>
-                                <li className='py-5 border__bottom border__default'>Home</li>
+                                <li className='py-5 border__bottom border__default'onClick={(e) => {
+                                    e.preventDefault();
+                                    setMobileMenuActive(false);
+                                    history.push("/home");
+                                }}>Home</li>
                                 <li className='py-5 border__bottom border__default' onClick={(e) => {
                                     e.preventDefault();
                                     !showSignIn && dispatch(actions.showHideSignUp(true));
+                                    setMobileMenuActive(false);
                                     dispatch(actions.setRegisteredUserType(DRIVER_TYPE));
                                 }}>Become a Driver</li>
                             </ul>
